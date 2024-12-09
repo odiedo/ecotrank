1b4403f0944218ae4ab5a30ac8196041de8cce04
odiedo <odiedopaul@gmail.com>
2024-12-09 18:56:35 +0300

    second update

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,6 +152,61 @@
     }
   });
 
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      center: true,
+      dots: false,
+      loop: true,
+      nav : true,
+      navText : [
+          '<i class="bi bi-chevron-left"></i>',
+          '<i class="bi bi-chevron-right"></i>'
+      ],
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+  });
+
+    // Client carousel
+    $(".client-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      margin: 90,
+      dots: false,
+      loop: true,
+      nav : false,
+      responsive: {
+          0:{
+              items:2
+          },
+          576:{
+              items:3
+          },
+          768:{
+              items:4
+          },
+          992:{
+              items:5
+          },
+          1200:{
+              items:6
+          }
+      }
+  });
+
   /**
    * Navmenu Scrollspy
    */
